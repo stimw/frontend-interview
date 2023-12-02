a01f511e7136c95280f6c948539722de14fb6f45
stimw <zhiwai98@gmail.com>
2023-12-02 21:07:02 +1100

    handwrite compose

---
+++ handwrite/compose.js
@@ -0,0 +1,16 @@
+// compose(f1, f2, f3)(value) -> f1(f2(f3(value)))
+function compose(...funcs) {
+  if (funcs.length === 0) {
+    return (arg) => arg;
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0];
+  }
+
+  return funcs.reduce(
+    (a, b) =>
+      (...args) =>
+        a(b(...args))
+  );
+}

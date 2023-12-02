3514062220cd710eb12c4a01b907fecdd8a33809
stimw <zhiwai98@gmail.com>
2023-12-03 03:21:20 +1100

    question scope.js

---
+++ question/scope.js
@@ -0,0 +1,28 @@
+// Run in browser
+
+function Foo() {
+  getName = function () {
+    console.log(1);
+  };
+  return this;
+}
+
+Foo.getName = function () {
+  console.log(2);
+};
+
+Foo.prototype.getName = function () {
+  console.log(3);
+};
+
+var getName = function () {
+  console.log(4);
+};
+
+function getName() {
+  console.log(5);
+}
+
+getName(); // 4
+Foo().getName(); // 1
+getName(); // 1
